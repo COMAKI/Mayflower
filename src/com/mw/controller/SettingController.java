b12461b43e2cf570bdd34db6ca25f5b7baef4f62
YoonKiSeok <yks5313@gmail.com>
2018-07-24 14:23:31 +0900

    API data call link add /callAPI.mw

---
+++ src/com/mw/controller/SettingController.java
@@ -0,0 +1,154 @@
+package com.mw.controller;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mw.dao.mapper.SpotMapper;
+import com.mw.vo.Spot;
+
+@Controller
+public class SettingController {
+
+	@Autowired
+	SpotMapper mapper;
+
+	@RequestMapping("/callAPI")
+	public String callAPI() {
+		// checkData exist
+		configData(new Spot());
+
+		System.out.println("success");
+		return "main"; // main.jsp
+	}
+
+	private void configData(Object target) {
+		// TODO Call target API
+		if (target instanceof Spot) {
+			ArrayList<Spot> spots = dataLoad("json");
+			for (int i = 0; i < spots.size(); i++) {
+				mapper.insert(spots.get(i));	
+			}
+			
+		}
+
+	}
+
+	private ArrayList<Spot> dataLoad(String type) {
+		int resultCode = 0;
+		ArrayList<Spot> result = new ArrayList<>();
+
+		String key = "UtE%2B%2BpVIVK2RQksQsXFcWWWHEqj7TZDaE46kR3l9fY64c%2BMx7n13gZSOJp%2BpeXBvIKDj1nt7nZuQLRUFqrU1ZQ%3D%3D";
+		try {
+			StringBuilder urlBuilder = new StringBuilder("http://api.data.go.kr/openapi/pblic-toilet-std"); /* URL */
+			urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "="
+					+ key); /* Service Key */
+			urlBuilder.append("&" + URLEncoder.encode("s_page", "UTF-8") + "="
+					+ URLEncoder.encode("1", "UTF-8")); /* 페이지 번호 */
+			urlBuilder.append("&" + URLEncoder.encode("s_list", "UTF-8") + "="
+					+ URLEncoder.encode("100", "UTF-8")); /* 페이지 크기 */
+			urlBuilder.append("&" + URLEncoder.encode("type", "UTF-8") + "="
+					+ URLEncoder.encode(type, "UTF-8")); /* XML/JSON 여부 */
+			URL url = new URL(urlBuilder.toString());
+
+			// connect API
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Content-type", "application/json");
+
+			BufferedReader rd;
+			JSONArray data;
+
+			if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+
+				resultCode = conn.getResponseCode();
+				System.out.println("Response code: " + resultCode);
+
+				rd = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+
+				data = (JSONArray) new JSONParser().parse(rd.readLine());
+
+				Iterator<JSONArray> iterator = data.iterator();
+				ArrayList<Spot> list = new ArrayList<>();
+
+			} else {
+				//// TODO default data input
+				rd = new BufferedReader(new InputStreamReader(new FileInputStream("data1.json"), "UTF8"));
+
+				JSONObject jsonObject = (JSONObject) new JSONParser().parse(rd);
+
+				// property setting
+				String columnName = "fields";
+				String rowName = "records";
+
+				// loop array
+				JSONArray columns = (JSONArray) jsonObject.get(columnName);
+				data = (JSONArray) jsonObject.get(rowName);
+				Iterator<JSONObject> iterator = data.iterator();
+			}
+
+			// input data
+			for (int i = 0; i < data.size(); i++) {
+				// data type mapping
+				result.add(mappingSpotData((JSONObject) data.get(i)));
+			}
+
+			// end
+			rd.close();
+			conn.disconnect();
+
+		} catch (Exception e)
+
+		{
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+	private Spot mappingSpotData(JSONObject value) {
+		Spot spot = new Spot();
+
+		// TODO : all data check null
+		// String[] columns = {"화장실명", "category", "구분", "소재지도로명주소", "소재지지번주소",
+		// "개방시간", "전화번호"};
+		String defaultValue = "추후 업데이트..";
+		Object lat = value.get("위도");
+		Object lng = value.get("경도");
+
+		// not valuable data
+		if (lat == null || lng == null)
+			return null;
+
+		spot.setName(value.get("화장실명").toString());
+		spot.setCategory_id("화장실");
+		spot.setSubcategory_id(getJSONObjectValue(value.get("구분").toString(), defaultValue));
+		spot.setAddress1(getJSONObjectValue(value.get("소재지도로명주소"), defaultValue));
+		spot.setAddress2(getJSONObjectValue(value.get("소재지지번주소"), defaultValue));
+		spot.setPhone(getJSONObjectValue(value.get("전화번호"), defaultValue));
+		spot.setLat(Double.parseDouble(lat.toString()));
+		spot.setLng(Double.parseDouble(lng.toString()));
+		spot.setRegdate(new Date().toString());
+
+		System.out.println(spot.toString());
+
+		return spot;
+	}
+
+	private String getJSONObjectValue(Object value, String defaultValue) {
+		return value == null ? defaultValue == null ? " " : defaultValue : value.toString();
+	}
+}
