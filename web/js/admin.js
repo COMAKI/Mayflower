c3f8f439870deb8caba44f6fe4a4cb1c22dfdfbf
pseudotop <lsupertopl@gmail.com>
2018-07-25 12:54:33 +0900

    Update ADMIN setting

--- web/js/admin.js
+++ web/js/admin.js
@@ -2,33 +2,60 @@
  *  ADMIN FUNCTION
  */
 document.charset="euc-kr";
-$('.btn:eq(0)').on({
-		click: function(e){
+$('.btn-admin').on({
+	click: function(e) {
 			console.log('requestAPI button clicked');
-			$.ajax({
-				url: 'requestAPI.mw',
-				method: 'GET',
-				success: (data)=>{
-					$("#RequestResult").modal('show');
-					var str = '';
-					if (data.status === 'fail') {
-						str += '<h3 style="color:#FE5F55;">'
-								+ 'Cannot REQUEST API on data.go.kr'
-								+'</h3>';
-						
-					} else if (data.status === 'pass') {
-						str += '<h3>REQUEST RESULT</h3>'
-								+'<h4>REQUEST COUNT: '+ data.count+'</h>';
-					};
-					$('#RequestResult .modal-body').html(
-							str
-					);
-				},
-				error: (err)=>{
-					console.log(err);
-				},
-				dataType: 'json'
-            });
+			var index = $( '.btn-admin' ).index( this );
+			console.log('btn-admin index: '+index);
+			if(index == 0){
+				$('#RequestResult .modal-body').html(
+						'<h3> Waiting for a result </h3>'
+				);
+				$("#RequestResult").modal('show');
+				$.ajax({
+					url: 'requestAPI.mw',
+					method: 'GET',
+					success: (data)=>{
+						var str = '';
+						if (data.status === 'fail') {
+							str += '<h3 style="color:#FE5F55;">'
+									+ 'Cannot REQUEST API on data.go.kr'
+									+'</h3>';
+							
+						} else if (data.status === 'pass') {
+							str += '<h3>REQUEST RESULT</h3>'
+									+'<h4>REQUEST COUNT: '+ data.count+'</h>';
+						};
+						$('#RequestResult .modal-body').html(
+								str
+						);
+					},
+					error: (err)=>{
+						console.log(err);
+					},
+					dataType: 'json'
+                });
+			}
             e.preventDefault(); // avoid to execute the actual submit of the form.
+		},
+		mouseenter: function(){
+			$(this).css({
+				"color":"white",
+				"background-color": "#28a745",
+				"border-color": "#1e7e34"
+			});
+			$(this).fadeOut(100);
+			$(this).fadeIn(500);
+		},
+		mouseleave: function(){
+			$(this).css({
+				"color":"black",
+				"background-color": "#dddddd",
+				"border-color": "#dddddd"
+			});
 		}
 });
+/*,
+hover: function(){
+	console.log('hover');
+}*/
